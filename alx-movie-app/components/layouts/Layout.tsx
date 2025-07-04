d217e247a91564ced792b86ddd6ccdcf6c149ff7
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-07-04 21:41:16 +0300

    landing page

--- alx-movie-app/components/layouts/Layout.tsx
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -1,12 +1,17 @@
+import { ComponentProps } from "@/interfaces";
 
 
+import Header from "./Header";
+import Footer from "./Footer";
 
-
-
-
-const Layout:React.FC=()=>{
-    return (
-        <></>
-    );
+const Layout: React.FC<ComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  )
 }
+
 export default Layout;
