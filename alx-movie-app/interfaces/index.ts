9ceabe761d4075aaf46ea3544a97c67a680e0ac3
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-07-04 21:52:46 +0300

    final

--- alx-movie-app/interfaces/index.ts
+++ alx-movie-app/interfaces/index.ts
@@ -1,5 +1,3 @@
-
-
 import { ReactNode } from "react";
 
 export interface ComponentProps {
@@ -9,4 +7,30 @@ export interface ComponentProps {
 export interface ButtonProps {
   title: string
   action?: () => void
+}
+
+export interface MovieProps {
+  id?: string
+  posterImage: string
+  releaseYear: string
+  title: string
+}
+
+interface PrimaryImage {
+  url: string
+}
+
+interface TitleText {
+  text: string
+}
+
+interface ReleaseYear {
+  year: string
+}
+
+export interface MoviesProps {
+  id: string
+  primaryImage: PrimaryImage
+  titleText: TitleText
+  releaseYear: ReleaseYear
 }
