d2a97dda67c10c89dfe6c928f8092c19078af53d
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-07-06 21:09:28 +0300

    -documment.tsx

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -1,9 +1,34 @@
+// pages/_document.tsx
+
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* PWA Metadata */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+
+        {/* PWA Icons */}
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/apple-icon-152x152.png"
+        />
+        <link 
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/icons/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="310x310"
+          href="/icons/ms-icon-310x310.png"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
